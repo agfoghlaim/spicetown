b615b68b3ef306612113cc2c11c3c472dd4115f9
agfoghlaim <marieohara.gti@gmail.com>
2019-01-01 07:37:39 +0000

    added stripe keys, charges views and ctrl, works fine local

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,34 @@
+class ChargesController < ApplicationController
+
+  def new
+    #will need a display amt and an amt in cents
+    #will need access to cart total, use helper in application ctrl
+    @amount = (current_cart.cart_total*100).to_i
+  end
+  
+  def create
+    #@cart =  current_cart
+    # Amount in cents, will need display amount too
+     @amount = (current_cart.cart_total*100).to_i
+     puts "amt is : #{@amount} and: #{@cart}"
+  
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source  => params[:stripeToken]
+    )
+  
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => 'Rails Stripe customer',
+      :currency    => 'usd'
+    )
+  
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
+  end
+  
+
+  
+end
